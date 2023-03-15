e2849a7f3296c46ff579410e87866db9dd242df1
CodeChrisB <c.buchberger01@gmail.com>
2023-03-15 23:26:50 +0100

    WIP Context Menu for Fileviewer

--- src/views/FileViewer/FileViewer.vue
+++ src/views/FileViewer/FileViewer.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-div(style="max-height:90vh").view.ma-2.overflow-x-hidden
+div(style="max-height:90vh;min-height:90vh").view.ma-2.overflow-x-hidden
   v-row().overflow-auto
     v-card.mb-3.container(
-      
       v-for="(fileContent) in files"
       @click="onFileClick(fileContent)"
+      @contextmenu="show($event,fileContent)"
     )
       div(v-if="['jpg','png'].includes(fileContent.suffix)")
         ViewerImage(:path="fileContent.fullPath")
@@ -14,10 +14,23 @@ div(style="max-height:90vh").view.ma-2.overflow-x-hidden
           v-icon(style="transform:scale(2)") {{fileContent.isFolder=== 1 ?  'mdi-folder': 'mdi-file-document-outline'}}
         v-row.d-flex.justify-center
           span.text-caption(style="word-break: break-word") {{fileContent.name}}
+          
+    v-menu(v-model="showMenu" :position-x="position.x" :position-y="position.y" absolute offset-y)
+      v-list
+        v-list-item(
+          v-for="prop in menu"
+          @click="prop.func(clickedFile)"
+        )
+          span {{ prop.title }}
 </template>
 
 <script>
-
+const State = Object.freeze({
+  FOLDER:0,
+  JSON:1,
+  IMAGE:2,
+  SWF:3
+})
 import { SETTING } from '@/assets/data/SettingData'
 import ViewerImage from './ViewerImage.vue'
 export default {
@@ -27,8 +40,20 @@ export default {
   },  
   data() {
     return {
+      position:{
+        x:0,
+        y:0
+      },
+      menu:[
+        {
+          title:'Open',
+          func: (game) => this.onFileClick(game)
+
+        }
+      ],
       files: [],
-      images:{}
+      images:{},
+      showMenu:false
     }
   },
 
@@ -71,10 +96,20 @@ export default {
         this.$router.pass('MonacoEditor', { key: this.clickedFile })
       }
     },
-
     onFinishedLoading(){
       console.log('onFinishedLoading',this.files)
-    }
+    },
+    show(e,file) {
+    e.preventDefault();
+    console.log(e)
+      this.showMenu = false;
+      this.position.x = e.clientX;
+      this.position.y = e.clientY;
+      this.clickedFile = file
+      this.$nextTick(() => {
+        this.showMenu = true;
+      });
+    },
   },
   watch: {
     "$route.params.key": {
